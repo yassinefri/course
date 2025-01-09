769fad2cdc2bee6dc5bf3d998ae751a9724cc65c
“[firstname <yassine.yassine@ynov.com>
2025-01-09 12:40:37 +0100

    test

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from termcolor import colored
 import pyfiglet
 
 ascii_art = pyfiglet.figlet_format("Hello, World!")
-colored_ascii = colored(ascii_art, 'cyan')
+colored_ascii = colored(ascii_art, "cyan")
 
 print(colored_ascii)
